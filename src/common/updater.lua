6b5cc4b871517592d500635f537452504d40f555
Ben Garren <bngarren@gmail.com>
2025-01-24 09:13:49 -0500

    wip

--- src/common/updater.lua
+++ src/common/updater.lua
@@ -4,17 +4,29 @@ local updater = {}
 function updater.generateStartup(installDir, programName, baseURL, installManifestFile)
     local startupContent = [[
 package.path = "/bng/common/?.lua;" .. package.path
-local updater = require("updater")
+local http = require("http")
 
-local manifestPath = "]] .. installManifestFile .. [["
-local baseURL = "]] .. baseURL .. [["
-local mainScript = "]] .. installDir .. [[main.lua"
+local function fetchRemoteManifest(url)
+    local response = http.get(url)
+    if not response then return nil end
+    local content = response.readAll()
+    response.close()
+    return textutils.unserializeJSON(content)
+end
+
+local function fetchLocalManifest(path)
+    if not fs.exists(path) then return nil end
+    local file = fs.open(path, "r")
+    local manifest = textutils.unserializeJSON(file.readAll())
+    file.close()
+    return manifest
+end
 
 local function checkForUpdates()
-    local remoteVersion = fetchRemoteManifest()
-    local localVersion = fetchLocalManifest()
+    local remoteManifest = fetchRemoteManifest("]] .. baseURL .. [[manifest.json")
+    local localManifest = fetchLocalManifest("]] .. installManifestFile .. [[")
 
-    if remoteVersion and localVersion and remoteVersion ~= localVersion then
+    if remoteManifest and localManifest and remoteManifest.version ~= localManifest.version then
         print("A new version is available. Updating...")
         shell.run("installer.lua", "]] .. programName .. [[")
         os.reboot()
@@ -22,10 +34,10 @@ local function checkForUpdates()
 end
 
 checkForUpdates()
-shell.run(mainScript)
+shell.run("]] .. installDir .. [[main.lua")
 ]]
 
-    local startupFile = fs.open(installDir .. "startup.lua", "w")
+    local startupFile = fs.open("/startup.lua", "w")
     startupFile.write(startupContent)
     startupFile.close()
 end
